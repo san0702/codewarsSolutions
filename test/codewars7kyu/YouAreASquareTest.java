250731bc943762047621272f1ea214cc0be3e27c
sandra <sandra.schueler@haw-hamburg.de>
2021-01-03 22:01:45 +0100

    added new task

---
+++ test/codewars7kyu/YouAreASquareTest.java
@@ -0,0 +1,17 @@
+package codewars7kyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class YouAreASquareTest {
+    @Test
+    public void shouldWorkForSomeExamples() throws Exception {
+        assertEquals("negative numbers aren't square numbers", false, YouAreASquare.isSquare(-1));
+        assertEquals("0 is a square number (0 * 0)",    true,   YouAreASquare.isSquare(0));
+        assertEquals("3 isn't a square number", false,  YouAreASquare.isSquare(3));
+        assertEquals("4 is a square number (2 * 2)",    true,   YouAreASquare.isSquare(4));
+        assertEquals("25 is a square number (5 * 5)",   true,   YouAreASquare.isSquare(25));
+        assertEquals("26 isn't a square number",false,  YouAreASquare.isSquare(26));
+    }
+}

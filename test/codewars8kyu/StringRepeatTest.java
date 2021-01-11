33fec5adaae974462001d8855ac9be489ac54b22
sandra <sandra.schueler@haw-hamburg.de>
2021-01-11 13:20:03 +0100

    added new tests for second method

--- test/codewars8kyu/StringRepeatTest.java
+++ test/codewars8kyu/StringRepeatTest.java
@@ -18,4 +18,18 @@ public class StringRepeatTest {
     @Test public void test0kata() {
         assertEquals("", StringRepeat.repeatStr(0, "kata"));
     }
+
+    @Test
+    public void test4a2() {
+        assertEquals("aaaa", StringRepeat.repeatStr2( 4, "a"));
+    }
+    @Test public void test3Hello2() {
+        assertEquals("HelloHelloHello", StringRepeat.repeatStr2(3, "Hello"));
+    }
+    @Test public void test5empty2() {
+        assertEquals("", StringRepeat.repeatStr2(5, ""));
+    }
+    @Test public void test0kata2() {
+        assertEquals("", StringRepeat.repeatStr2(0, "kata"));
+    }
 }

20e6d82e099dd062a74dc06161a0022641b45d9e
sandra <sandra.schueler@haw-hamburg.de>
2022-07-22 21:56:39 +0200

    kata and solution

---
+++ test/codewars8kyu/FindMaximumAndMinimumValuesOfAListTest.java
@@ -0,0 +1,17 @@
+package codewars8kyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FindMaximumAndMinimumValuesOfAListTest {
+    private FindMaximumAndMinimumValuesOfAList kata = new FindMaximumAndMinimumValuesOfAList();
+
+    @Test
+    public void testExamples() {
+        assertEquals(-110, kata.min(new int[]{-52, 56, 30, 29, -54, 0, -110}));
+        assertEquals(0, kata.min(new int[]{42, 54, 65, 87, 0}));
+        assertEquals(566, kata.max(new int[]{4,6,2,1,9,63,-134,566}));
+        assertEquals(5, kata.max(new int[]{5}));
+    }
+}

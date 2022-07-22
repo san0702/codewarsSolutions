72ce96b5ecdf15d9f5ed4f3328f6bbb7ea950d04
sandra <sandra.schueler@haw-hamburg.de>
2022-07-22 21:52:27 +0200

    add new solution

--- test/codewars8kyu/AllStarCodeChallenge18Test.java
+++ test/codewars8kyu/AllStarCodeChallenge18Test.java
@@ -10,6 +10,7 @@ public class AllStarCodeChallenge18Test {
         assertEquals(1, AllStarCodeChallenge18.strCount("Hello", 'o'));
         assertEquals(2, AllStarCodeChallenge18.strCount("Hello", 'l'));
         assertEquals(0, AllStarCodeChallenge18.strCount("",'z'));
+        assertEquals(4, AllStarCodeChallenge18.strCount("aaabcgda", 'a'));
     }
 
     @Test
